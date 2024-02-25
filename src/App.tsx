5a6b9de10da2f6a373d965cc4e477e05be7d0182
Kanzaki Urumi <urkanzak1@mail.ru>
2024-02-25 22:38:09 +0400

    Revert "commit 1 to revert"
    This reverts commit c26c5ebbea1f6fb86a78cff217a38f7f072a8c92.

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import { Provider } from 'react-redux';
 import { TodoStore } from './redux/TodoStore';
 import { Loader } from './components/loader/Loader';
 
-//commit 1 to revert
-//commit 2 to revert
 function App() { 
   return (
     <Provider store={TodoStore}>
