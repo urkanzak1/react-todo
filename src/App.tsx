3bcc082ac19f2512d1321933a82b604fe7c421e3
Kanzaki Urumi <urkanzak1@mail.ru>
2024-02-25 22:43:23 +0400

    commit 4 to reset

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ import { Provider } from 'react-redux';
 import { TodoStore } from './redux/TodoStore';
 import { Loader } from './components/loader/Loader';
 
+<<<<<<< HEAD
+=======
+//commit 3 to reset
+//commit 4 to reset
+>>>>>>> 0500de3 (commit 4 to reset)
 function App() { 
   return (
     <Provider store={TodoStore}>
